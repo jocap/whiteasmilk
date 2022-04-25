6924d250ad590efec806f92a53c8b3aa7dbdc609
John Ankarström <john@ankarstrom.se>
2022-04-26 00:03:38 +0200

    Don't enqueue comment-reply.js.
    It doesn't seem to work, so I'm removing it for now...

--- functions.php
+++ functions.php
@@ -6,13 +6,10 @@ if ( function_exists('register_sidebar') )
         'before_title' => '<h2 class="widgettitle">',
         'after_title' => '</h2>',
     ));
-if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-    wp_enqueue_script( 'comment-reply' );
-}
-function wps_deregister_styles() {
+function wam_deregister_styles() {
     wp_dequeue_style( 'global-styles' );
     wp_dequeue_style( 'wp-block-library' );
     wp_dequeue_style( 'wp-block-library-theme' );
 }
-add_action( 'wp_enqueue_scripts', 'wps_deregister_styles', 100 );
+add_action( 'wp_enqueue_scripts', 'wam_deregister_styles', 100 );
 ?>
