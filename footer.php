3b32e151fab07655fcb1f7e1380f17a922107719
John Ankarström <john@ankarstrom.se>
2022-04-25 23:19:08 +0200

    Update footer links.

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <div id="footer">
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
 	<p>
-		<?php bloginfo('name'); ?> is powered by <a href="http://wordpress.org/">WordPress</a> with <a href="http://www.azeemazeez.com/blogs/white-as-milk/" title="White as Milk theme for Wordpress">White as Milk</a> <br />designed by <a href="http://www.azeemazeez.com">Azeem Azeez</a>. <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+		<?php bloginfo('name'); ?> is powered by <a href="http://wordpress.org/">WordPress</a> with <a href="https://github.com/jocap/whiteasmilk" title="White as Milk theme for WordPress">White as Milk</a> <br />designed by <a href="https://azeem.me/">Azeem Azeez</a>. <a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
 		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 	</p>
 </div>
