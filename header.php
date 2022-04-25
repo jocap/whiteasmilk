1d4c0d268875a68d87486d7dd9e458b01499fb10
John Ankarström <john@ankarstrom.se>
2022-04-25 22:40:01 +0200

    Compatibility with PHP 7.
    This also disables the mobile stylesheet.

--- header.php
+++ header.php
@@ -5,10 +5,6 @@
 <title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Blog Archive <?php } ?> <?php wp_title(); ?></title>
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-<?php $agent = $_SERVER['HTTP_USER_AGENT'];
-if (eregi("BlackBerry", $agent)) {?>
-<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/mobile.css" type="text/css" media="screen" />
-<?php } ?>
 
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
